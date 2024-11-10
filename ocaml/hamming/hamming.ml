780fc71465d08099cf0c388cd81379483673675c
Marc Fong <marcjohnfong@gmail.com>
2024-11-10 19:16:58 +0800

    Add iteration for ocaml/hamming

--- ocaml/hamming/hamming.ml
+++ ocaml/hamming/hamming.ml
@@ -1,17 +1,16 @@
 type nucleotide = A | C | G | T
 
-let hamming_distance d1 d2 =
-  match (d1, d2) with
-  | [], [] -> Ok 0
-  | [], _ -> Error "left strand must not be empty"
-  | _, [] -> Error "right strand must not be empty"
-  | d1, d2 ->
-      if List.length d1 <> List.length d2 then
-        Error "left and right strands must be of equal length"
-      else
-        let count =
-          List.combine d1 d2
-          |> List.filter (fun (x1, x2) -> x1 <> x2)
-          |> List.length
-        in
-        Ok count
+let hamming_distance left right =
+  let rec aux d1 d2 result =
+    match (d1, d2, result) with
+    | _, _, Error _ -> result
+    | [], [], _ -> result
+    | [], _, Ok 0 -> Error "left strand must not be empty"
+    | _, [], Ok 0 -> Error "right strand must not be empty"
+    | [], _, _ -> Error "left and right strands must be of equal length"
+    | _, [], _ -> Error "left and right strands must be of equal length"
+    | x1 :: d1, x2 :: d2, Ok count ->
+        let count = if x1 <> x2 then count + 1 else count in
+        aux d1 d2 (Ok count)
+  in
+  aux left right (Ok 0)
