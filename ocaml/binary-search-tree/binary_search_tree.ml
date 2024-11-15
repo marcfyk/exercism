b0bf081e040b6ef60c5d3e410a9c20943ee01100
Marc Fong <marcjohnfong@gmail.com>
2024-11-15 19:20:04 +0800

    Add iteration for ocaml/binary-search-tree

--- ocaml/binary-search-tree/binary_search_tree.ml
+++ ocaml/binary-search-tree/binary_search_tree.ml
@@ -1,17 +1,19 @@
-type node = Node of int * node option * node option
-type bst = node option
+type bst = None | Node of int * bst * bst
 
 let empty = None
-let value = function None -> Error "" | Some (Node (n, _, _)) -> Ok n
-let left = function None -> Error "" | Some (Node (_, left, _)) -> Ok left
-let right = function None -> Error "" | Some (Node (_, _, right)) -> Ok right
+let value = function None -> Error "" | Node (n, _, _) -> Ok n
+let left = function None -> Error "" | Node (_, left, _) -> Ok left
+let right = function None -> Error "" | Node (_, _, right) -> Ok right
 
 let rec insert value = function
-  | None -> Some (Node (value, None, None))
-  | Some (Node (n, left, right)) ->
-      if value <= n then Some (Node (n, insert value left, right))
-      else Some (Node (n, left, insert value right))
+  | None -> Node (value, None, None)
+  | Node (n, left, right) ->
+      if value <= n then Node (n, insert value left, right)
+      else Node (n, left, insert value right)
 
-let rec to_list = function
-  | None -> []
-  | Some (Node (n, left, right)) -> to_list left @ (n :: to_list right)
+let to_list =
+  let rec build_list acc = function
+    | None -> acc
+    | Node (n, left, right) -> build_list (n :: build_list acc right) left
+  in
+  build_list []
