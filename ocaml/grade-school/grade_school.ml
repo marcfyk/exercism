4a0a655bb40e7e643568ca5802faebea6d4149f2
Marc Fong <marcjohnfong@gmail.com>
2024-11-18 23:15:21 +0800

    Add iteration for ocaml/grade-school

--- ocaml/grade-school/grade_school.ml
+++ ocaml/grade-school/grade_school.ml
@@ -4,27 +4,7 @@ module Int_map = Map.M (Int)
 type school = string list Int_map.t
 
 let empty_school = Map.empty (module Int)
-
-let add student grade school =
-  let f = function
-    | None -> [ student ]
-    | Some v -> (
-        match List.find v ~f:(String.equal student) with
-        | None -> student :: v
-        | Some _ -> v)
-  in
-  Map.update school grade ~f
-
-let grade grade school = Map.find school grade |> Option.value ~default:[]
-
-let sorted school =
-  let f ~key ~data results =
-    Map.add_exn results ~key ~data:(List.sort data ~compare:String.compare)
-  in
-  Map.fold school ~init:empty_school ~f
-
-let roster school =
-  let f ~key:_ ~data results =
-    List.sort data ~compare:String.compare @ results
-  in
-  Map.fold_right school ~init:[] ~f
+let add student grade school = Map.add_multi ~key:grade ~data:student school
+let grade grade school = Map.find_multi school grade
+let sorted school = Map.map school ~f:(List.sort ~compare:String.compare)
+let roster school = sorted school |> Map.data |> List.bind ~f:Fn.id
